d0fcd8801d8b8ce559bb0df4b8339c2428729d13
Bilawal-Mehfooz-Malik <bilawalmehfoozflutter@gmail.com>
2024-09-19 14:33:50 +0500

    added project data

---
+++ lib/data/questions.dart
@@ -0,0 +1,58 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'How many daily prayers are there in Islam?',
+    [
+      'Five', // Correct answer
+      'Three',
+      'Four',
+      'Six',
+    ],
+  ),
+  QuizQuestion(
+    'What is the holy book of Islam?',
+    [
+      'Quran', // Correct answer
+      'Bible',
+      'Torah',
+      'Vedas',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the final prophet in Islam?',
+    [
+      'Prophet Muhammad (PBUH)', // Correct answer
+      'Prophet Isa (Jesus)',
+      'Prophet Musa (Moses)',
+      'Prophet Ibrahim (Abraham)',
+    ],
+  ),
+  QuizQuestion(
+    'In which month do Muslims fast?',
+    [
+      'Ramadan', // Correct answer
+      'Muharram',
+      'Dhul-Hijjah',
+      'Shawwal',
+    ],
+  ),
+  QuizQuestion(
+    'What is the first pillar of Islam?',
+    [
+      'Shahada', // Correct answer
+      'Salat',
+      'Zakat',
+      'Hajj',
+    ],
+  ),
+  QuizQuestion(
+    'Where do Muslims face when they pray?',
+    [
+      'Kaaba in Mecca', // Correct answer
+      'Medina',
+      'Jerusalem',
+      'Mount Sinai',
+    ],
+  ),
+];
